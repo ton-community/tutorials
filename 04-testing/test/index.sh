0eeec2938a64841d2a7924b935fbd01276bc1596
Tal Kol <talkol@gmail.com>
2023-01-27 17:39:35 +0000

    Improved tutorial 4 test

--- 04-testing/test/index.sh
+++ 04-testing/test/index.sh
@@ -5,4 +5,5 @@ npm install ton-core @ton-community/sandbox @ton-community/test-utils
 npx jest step2
 npx jest step3
 npx jest step4
-npx jest step5
+npx jest step5 | grep "#DEBUG#" > step5.output.txt
+diff step5.output.txt step5.expected.txt
