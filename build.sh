a3e897a686037ee5dfc3c21e1c5d6a7c6baeeb41
Shahar Yakir <shahar.yakir@gmail.com>
2023-05-11 17:31:40 +0300

    change deprecated npm set-script to pkg set.scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 npm init --yes
 npm install ts-node
-npm set-script watch "sass -w styles.scss docs/assets/styles.css"
+npm pkg set scripts.watch="sass -w styles.scss docs/assets/styles.css"
 npm install ejs @types/ejs
 npm install sass
 npm install showdown @types/showdown
